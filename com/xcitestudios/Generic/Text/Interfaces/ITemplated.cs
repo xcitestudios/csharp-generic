f047f12ddd48e8596bc53f4ace0c52e13cee79ee
Wade Womersley <wade85@gmail.com>
2015-02-20 19:07:34 +0000

    Template Interfaces

---
+++ com/xcitestudios/Generic/Text/Interfaces/ITemplated.cs
@@ -0,0 +1,24 @@
+﻿namespace com.xcitestudios.Generic.com.xcitestudios.Generic.Text.Interfaces
+{
+    /// <summary>
+    /// General interface for something that can render a template style string.
+    /// </summary>
+    public interface ITemplated
+    {
+        /// <summary>
+        /// Template version of the content to be used during rendering.
+        /// </summary>
+        string Content { get; set; }
+
+        /// <summary>
+        /// Object passed in to replace items in the content.
+        /// </summary>
+        object Context { get; set; }
+
+        /// <summary>
+        /// Renders content given the context object.
+        /// </summary>
+        /// <returns>String content rendered using the values in <see cref="P:context"/></returns>
+        string Render();
+    }
+}
---
+++ com/xcitestudios/Generic/Text/Interfaces/ITemplatedSerializable.cs
@@ -0,0 +1,11 @@
+﻿namespace com.xcitestudios.Generic.com.xcitestudios.Generic.Text.Interfaces
+{
+    using global::com.xcitestudios.Generic.Data.Manipulation.Interfaces;
+
+    /// <summary>
+    /// General interface for something that can render a template style string and supports serialization.
+    /// </summary>
+    public interface ITemplatedSerializable : ITemplated, ISerialization
+    {
+    }
+}
