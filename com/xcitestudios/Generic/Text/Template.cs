8b2fe98cec207887d23214a450ab5de3eaff66a9
Wade Womersley <wade85@gmail.com>
2015-02-24 12:21:06 +0000

    Added Simple serializable template using mustache

---
+++ com/xcitestudios/Generic/Text/Template.cs
@@ -0,0 +1,62 @@
+﻿namespace com.xcitestudios.Generic.Text
+{
+    using com.xcitestudios.Generic.Data.Manipulation.Interfaces;
+    using com.xcitestudios.Generic.Text.Interfaces;
+    using Nustache.Core;
+    using System.IO;
+    using System.Runtime.Serialization;
+    using System.Web.Script.Serialization;
+
+    /// <summary>
+    /// A simple implementation of <see cref="I:com.xcitestudios.Generic.Text.Interfaces.ITemplated"/>
+    /// </summary>
+    [DataContract]
+    public class Template : ITemplatedSerializable, ISerialization
+    {
+        /// <summary>
+        /// Template version of the content to be used during rendering.
+        /// </summary>
+        [DataMember(Name="content")]
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Object passed in to replace items in the content.
+        /// </summary>
+        [DataMember(Name = "context")]
+        public object Context { get; set; }
+
+        /// <summary>
+        /// Renders content given the context object.
+        /// </summary>
+        /// <returns>String content rendered using the values in <see cref="P:context"/></returns>
+        public string Render()
+        {
+            return Nustache.Core.Render.StringToString(Content, Context);
+        }
+
+        /// <summary>
+        /// Updates the element implementing this interface using a JSON representation. 
+        /// This means updating the state of this object with that defined in the JSON 
+        /// as opposed to returning a new instance of this object.
+        /// </summary>
+        /// <param name="jsonString">Representation of the object</param>
+        public void DeserializeJSON(string jsonString)
+        {
+            var serializer = new JavaScriptSerializer();
+            var newObj = serializer.Deserialize<Template>(jsonString);
+
+            Content = newObj.Content;
+            Context = newObj.Context;
+        }
+
+        /// <summary>
+        /// Convert this object into JSON so it can be handled by anything that supports JSON.
+        /// </summary>
+        /// <returns>A JSON representation of this object</returns>
+        public string SerializeJSON()
+        {
+            var serializer = new JavaScriptSerializer();
+            return serializer.Serialize(this);
+        }
+    }
+}
---
+++ tests/com/xcitestudios/Generic/Text/TemplatedTest.cs
@@ -0,0 +1,50 @@
+﻿namespace tests.com.xcitestudios.Generic.Text
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using global::com.xcitestudios.Generic.Text;
+
+    [TestClass]
+    public class TemplatedTest
+    {
+        [TestMethod]
+        public void TestContext()
+        {
+            var contextObject = new
+            {
+                alpha = "ALPHA",
+                beta = "BETA",
+                trueCheck = true,
+                falseCheck = false
+            };
+
+            var template = new Template();
+            template.Content = "{{alpha}} {{beta}} {{#trueCheck}}trueCheck{{/trueCheck}} {{#falseCheck}}falseCheck{{/falseCheck}}";
+            template.Context = contextObject;
+
+            Assert.AreEqual("ALPHA BETA trueCheck ", template.Render());
+        }
+
+        [TestMethod]
+        public void TestSerializationConsistency()
+        {
+            var contextObject = new { 
+                alpha = "ALPHA",
+                beta = "BETA"
+            };
+
+            var template = new Template();
+            template.Content = "{{alpha}} {{beta}}";
+            template.Context = contextObject;
+
+            var json = template.SerializeJSON();
+
+            var newTemplate = new Template();
+            newTemplate.DeserializeJSON(json);
+
+            Assert.AreEqual(template.Content, newTemplate.Content);
+            Assert.AreEqual("ALPHA BETA", template.Render());
+            Assert.AreEqual("ALPHA BETA", newTemplate.Render());
+        }
+    }
+}
